d261cdfefb13e1f8de2603032d796395f78515c4
Oscar <oscardc.oedc@gmail.com>
2025-06-26 20:25:24 -0700

    Correccion de la funcion primos

--- src/Primos.java
+++ src/Primos.java
@@ -3,24 +3,28 @@ public class Primos {
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
         int numero;
+
         System.out.println("Ingrese un numero para comprobar si es par o impar");
         numero = entrada.nextInt();
-        impar(numero);
+
+        if (impar(numero)<=2){
+            System.out.println("El numero es primo");
+        } else {
+            System.out.println("El numero no es primo");
+        }
 
 
     }
-    public static void impar(int a){
+    public static int impar(int a){
         int numeros_divisibles = 0;
+
         for (int i=1; i<=a && numeros_divisibles<=2; i++){
             if(a % i == 0){
                 numeros_divisibles = numeros_divisibles + 1;
                 System.out.println(numeros_divisibles);
             }
         }
-        if (numeros_divisibles<=2){
-            System.out.println("El numero es primo");
-        } else {
-            System.out.println("El numero no es primo");
-        }
+
+        return numeros_divisibles;
     }
 }
