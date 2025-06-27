b1f9ae61cca0d003eb762d6f2c7a338e88c52310
Oscar <oscardc.oedc@gmail.com>
2025-06-26 19:50:45 -0700

    funcion para identificar si un numero es primo o no

---
+++ src/Primos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Primos {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        System.out.println("Ingrese un numero para comprobar si es par o impar");
+        numero = entrada.nextInt();
+        impar(numero);
+
+
+    }
+    public static void impar(int a){
+        int numeros_divisibles = 0;
+        for (int i=1; i<=a && numeros_divisibles<=2; i++){
+            if(a % i == 0){
+                numeros_divisibles = numeros_divisibles + 1;
+                System.out.println(numeros_divisibles);
+            }
+        }
+        if (numeros_divisibles<=2){
+            System.out.println("El numero es primo");
+        } else {
+            System.out.println("El numero no es primo");
+        }
+    }
+}
